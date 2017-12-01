6996a01289ae4659e8673150f651556a180ec76c
Chad Grant <chadgra@gmail.com>
2017-11-30 22:32:56 -0700

    Refactor the main version hero program so that it isn't in one big main function.

--- versionhero.py
+++ versionhero.py
@@ -4,10 +4,178 @@ Main versionhero program.
 import argparse
 import os
 import re
+import shutil
 from repo_details import RepoDetails
 
 
 KEY_CHARACTERS = r'[!@#$%^&*]'
+
+
+class VersionHero:
+    """
+    Main class the implements the file processing logic.
+    """
+    def __init__(self):
+        self.args = ProgramArgs()
+
+    def fetch_template_text(self):
+        """
+        Get the template text that will be keyword replaced
+        :return: a string with the template text
+        """
+        if self.args.rename():
+            return self.args.input_file()
+        else:
+            file = open(self.args.input_file(), 'r')
+            file_text = file.read()
+            file.close()
+            return file_text
+
+    def save_template_text(self, text):
+        """
+        Save the keyword replaced text to the correct location
+        :param text: the keyword replaced text
+        :return: None
+        """
+        if self.args.rename():
+            shutil.copyfile(self.args.input_file(), text)
+        else:
+            if os.path.exists(self.args.backup_file()):
+                os.remove(self.args.backup_file())
+
+            if os.path.exists(self.args.output_file()):
+                os.rename(self.args.output_file(), self.args.backup_file())
+
+            file = open(self.args.output_file(), 'w')
+            file.write(text)
+            file.close()
+
+    def execute(self):
+        """
+        Execute the VersionHero keyword replacement process
+        :return: None
+        """
+        repo = RepoDetails(self.args.repo_dir(),
+                           tag_prefix=self.args.tag_prefix(),
+                           sub_paths=self.args.project_dir())
+        repo.print_summary()
+
+        text = self.fetch_template_text()
+        replacer = KeywordReplacer(text, repo)
+        new_text = replacer.execute()
+        self.save_template_text(new_text)
+
+
+class ProgramArgs:
+    """
+    Setup the args and validate them.
+    """
+    def __init__(self):
+        # Setup command-line args that we accept.
+        parser = argparse.ArgumentParser(description='Parse an input file, replacing tags with ' +
+                                                     'information about the git repository.')
+
+        parser.add_argument('template',
+                            help='The template file.')
+        parser.add_argument('--repo_dir', '-r', '-s', '--repo',
+                            help='The repository - defaults to the current repository',
+                            default='')
+        parser.add_argument('--project_dir', '-p', '--project',
+                            help='Directory of a project in a git repository.')
+        parser.add_argument('--tag_prefix', '-tp',
+                            help='The prefix of matching tags',
+                            default='')
+        parser.add_argument('--rename', '-rn',
+                            action='store_true')
+
+        self.args = parser.parse_args()
+        self._input_file = None
+        self._repo_dir = None
+        self._project_dir = None
+
+    def input_file(self):
+        """
+        Get the input file path
+        :return: the path of the input file as an absolute path
+        """
+        if self._input_file:
+            return self._input_file
+
+        input_file = self.args.template
+        if not os.path.isabs(input_file):
+            input_file = os.path.abspath(os.path.join(os.getcwd(), input_file))
+        input_file = input_file if input_file.endswith('.git') else input_file + '.git'
+
+        self._input_file = input_file
+        return self._input_file
+
+    def output_file(self):
+        """
+        Get the output file path
+        :return: the path of the output file as an absolute path
+        """
+        return self._input_file[:-len('.git')]
+
+    def backup_file(self):
+        """
+        Get the backup file path
+        :return: the path of the backup file as an absolute path
+        """
+        return self.output_file() + '.bak'
+
+    def repo_dir(self):
+        """
+        Get the repository directory
+        :return: the repository directory as an absolute path
+        """
+        if self._repo_dir:
+            return self._repo_dir
+
+        # Initialize the repo_dir if it was empty or a relative path.
+        repo_dir = self.args.repo_dir
+        if not repo_dir or not os.path.isabs(repo_dir):
+            repo_dir = os.path.abspath(os.path.join(os.getcwd(), repo_dir))
+
+        # Find the root directory of the repository by looking for a '.git' folder.
+        while not os.path.isdir(os.path.join(repo_dir, '.git')):
+            repo_dir = os.path.abspath(os.path.join(repo_dir, '..'))
+
+        self._repo_dir = repo_dir
+        return self._repo_dir
+
+    def project_dir(self):
+        """
+        Get the project directory.
+        :return: the project directory as a partial path.
+        """
+        if self._project_dir:
+            return self._project_dir
+
+        # Initialize the project_dir, which could be a sub-directory of the repository.
+        project_dir = self.args.project_dir
+        if not project_dir:
+            project_dir = self.repo_dir()
+        elif not os.path.isabs(project_dir):
+            project_dir = os.path.abspath(project_dir)
+        project_dir = project_dir.replace(self.repo_dir(), '')
+
+        self._project_dir = project_dir
+        return self._project_dir
+
+    def tag_prefix(self):
+        """
+        Get the tag_prefix
+        :return: the tag prefix as a string
+        """
+        return self.args.tag_prefix
+
+    def rename(self):
+        """
+        Get the rename bool
+        :return: True if the file name should be keyword exchanged, False if the files contents
+                 should be keyword exchanged
+        """
+        return self.args.rename
 
 
 class KeywordReplacer:
@@ -72,66 +240,8 @@ def main():
     Run this main function if this script is called directly.
     :return: None
     """
-    # Setup command-line args that we accept.
-    parser = argparse.ArgumentParser(description='Parse an input file, replacing tags with ' +
-                                                 'information about the git repository.')
-
-    parser.add_argument('--template', '-t', '-f',
-                        help='The template file.',
-                        required=True)
-    parser.add_argument('--repo_dir', '-r', '-s', '--repo',
-                        help='The repository - defaults to the current repository',
-                        default='')
-    parser.add_argument('--project_dir', '-p', '--project',
-                        help='Directory of a project in a git repository.')
-    parser.add_argument('--tag_prefix', '-tp',
-                        help='The prefix of matching tags',
-                        default='')
-
-    args = parser.parse_args()
-
-    # Initialize the input and output files.
-    input_file = args.template
-    if not os.path.isabs(input_file):
-        input_file = os.path.abspath(os.path.join(os.getcwd(), input_file))
-    input_file = input_file if input_file.endswith('.git') else input_file + '.git'
-    output_file = input_file[:-len('.git')]
-    backup_file = output_file + '.bak'
-
-    # Initialize the repo_dir if it was empty or a relative path.
-    repo_dir = args.repo_dir
-    if not repo_dir or not os.path.isabs(repo_dir):
-        repo_dir = os.path.abspath(os.path.join(os.getcwd(), repo_dir))
-
-    # Find the root directory of the repository by looking for a '.git' folder.
-    while not os.path.isdir(os.path.join(repo_dir, '.git')):
-        repo_dir = os.path.abspath(os.path.join(repo_dir, '..'))
-
-    # Initialize the project_dir, which could be a sub-directory of the repository.
-    project_dir = args.project_dir
-    if not project_dir:
-        project_dir = repo_dir
-    elif not os.path.isabs(project_dir):
-        project_dir = os.path.abspath(project_dir)
-    project_dir = project_dir.replace(repo_dir, '')
-
-    repo = RepoDetails(repo_dir, tag_prefix=args.tag_prefix, sub_paths=project_dir)
-    repo.print_summary()
-
-    file = open(input_file, 'r')
-    file_text = file.read()
-    file.close()
-    replacer = KeywordReplacer(file_text, repo)
-
-    if os.path.exists(backup_file):
-        os.remove(backup_file)
-
-    if os.path.exists(output_file):
-        os.rename(output_file, backup_file)
-
-    file = open(output_file, 'w')
-    file.write(replacer.execute())
-    file.close()
+    version_hero = VersionHero()
+    version_hero.execute()
 
 
 if __name__ == "__main__":
